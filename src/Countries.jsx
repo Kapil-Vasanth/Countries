a50f7a563aff8f65d25e3e4b4e70253494499823
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-05-13 23:50:26 +0530

    Added Filtered Search Link

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,47 +1,77 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+function Country({ imgUrl, alt, text }) {
+  return (
+    <div
+      style={{
+        border: "1px solid grey",
+        borderRadius: "8px",
+        padding: "10px",
+        width: "150px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: "10px",
+      }}
+    >
+      <img
+        src={imgUrl}
+        alt={alt}
+        style={{ width: "100%", height: "100%", margin: "auto" }}
+      />
+      <h3 style={{ textAlign: "center", marginTop: "10px" }}>{text}</h3>
+    </div>
+  );
+}
+export default function Countries() {
+  const [countries, setCountries] = useState([]);
+  const [filterCountries, setFilterCountries] = useState([]);
+  const [searchInput, setSearchInput] = useState([]);
+  const API_URL = "https://restcountries.com/v3.1/all";
 
-export default function Countries(){
-    const [countries,setCountries] = useState([]);
-    const API_URL = 'https://restcountries.com/v3.1/all'
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+        setFilterCountries(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch(API_URL)
-        .then(res => res.json())
-        .then(data => setCountries(data))
-        .catch(err => console.log(err))
-    }, [])
-
-
-    function Country({imgUrl,alt,text}){
-        return (
-            <div style={{
-                border: "1px solid grey",
-                borderRadius: "8px",
-                padding: "10px",
-                width: "200px",
-                display:"flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                gap: "10px"
-            }}>
-                <img src={imgUrl} alt={alt} style={{width:"100%", height: "100%", margin:"auto"}}/>
-                <h3 style={{textAlign:"center", marginTop:"10px"}}>{text}</h3>
-            </div>
-        )
-    }
+  function handleSearch(val){
+    const filteredCountry = countries.filter(country => {
+        let text = '' + country.name.common
+        return text.toLowerCase().search(val) > -1
+    })
+    setFilterCountries(prev => filteredCountry)
+    setSearchInput(prev => val)
+  }
 
 
-    return (
-        <div style={{
-            display:"flex",
-            flexWrap:"wrap",
-            justifyContent:"center",
-            gap: "10px"
-        }}>
-            {countries.map(country => {
-                console.log(country.flag)
-                return <Country key={country.flag.cca3} imgUrl={country.flags.png} alt={country.flags.alt} text={country.name.common}/>
-            })}
-        </div>
-    )
+  return (
+    <>
+      <div className="inputSearchContainer">
+        <input className="inputSearch" type="search" name="q" value={searchInput} onChange={e => handleSearch(e.target.value)} />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          justifyContent:'center'
+        }}
+      >
+        {filterCountries.map((country) => (
+              <Country
+                key={country.flag.cca3}
+                imgUrl={country.flags.png}
+                alt={country.flags.alt}
+                text={country.name.common}
+              />
+            ))}
+      </div>
+    </>
+  );
 }
